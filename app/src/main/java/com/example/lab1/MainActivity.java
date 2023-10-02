a67316a3d17818b85cc2f178d1dc7570c4d80471
Szade407 <mirzohrabs@gmail.com>
2023-10-02 22:24:02 +0300

    added button for changing color

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -11,16 +11,33 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
     private Button changeTextButton;
+    private Button changeColorButton;
+    private boolean isTextColorChanged = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.textView);
+        changeColorButton = findViewById(R.id.changeColor);
         changeTextButton = findViewById(R.id.changeText);
         changeTextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 textView.setText("Text Changed!");
+            }
+        });
+
+        changeColorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Toggle the text color between red and black
+                if (!isTextColorChanged) {
+                    textView.setTextColor(getResources().getColor(android.R.color.holo_red_dark));
+                    isTextColorChanged = true;
+                } else {
+                    textView.setTextColor(getResources().getColor(android.R.color.black));
+                    isTextColorChanged = false;
+                }
             }
         });
     }
